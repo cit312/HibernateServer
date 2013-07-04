86d207cecde2b9394354e0fc36342fa0bcc2b7aa
brayden hancock <brayden.hancock@tvw.org>
2013-07-04 12:12:00 -0700

    Added ID attr for the pivots

--- src/getPivots.java
+++ src/getPivots.java
@@ -11,43 +11,43 @@ public class GetPivots implements Handler {
 		//Get the correct bean crap
 		HashMap<String,HashMap> pivots = new HashMap<String,HashMap>();
 		HashMap<String,String> attr = new HashMap<String,String>();
-		attr.put("Attr1", "It is 60 seconds late");
+		attr.put("ID", "1");
 		attr.put("Attr2", "Last changed on that day");
 		attr.put("Attr3", "60 degrees to the left");
 		pivots.put("Pivot 1", attr);
 		
 		attr = new HashMap<String,String>();
-		attr.put("Attr1", "It is 160 seconds late");
+		attr.put("ID", "2");
 		attr.put("Attr2", "changed on that day");
 		attr.put("Attr3", "160 degrees to the left");
 		pivots.put("Pivot 2", attr);
 		
 		attr = new HashMap<String,String>();
-		attr.put("Attr1", "It is 160 seconds late");
+		attr.put("ID", "3");
 		attr.put("Attr2", "changed on that day");
 		attr.put("Attr3", "160 degrees to the left");
 		pivots.put("Pivot 3", attr);
 		
 		attr = new HashMap<String,String>();
-		attr.put("Attr1", "It is 160 seconds late");
+		attr.put("ID", "4");
 		attr.put("Attr2", "changed on that day");
 		attr.put("Attr3", "160 degrees to the left");
 		pivots.put("Pivot 4", attr);
 		
 		attr = new HashMap<String,String>();
-		attr.put("Attr1", "It is 160 seconds late");
+		attr.put("ID", "5");
 		attr.put("Attr2", "changed on that day");
 		attr.put("Attr3", "160 degrees to the left");
 		pivots.put("Pivot 5", attr);
 		
 		attr = new HashMap<String,String>();
-		attr.put("Attr1", "It is 160 seconds late");
+		attr.put("ID", "6");
 		attr.put("Attr2", "changed on that day");
 		attr.put("Attr3", "160 degrees to the left");
 		pivots.put("Pivot 6", attr);
 		
 		attr = new HashMap<String,String>();
-		attr.put("Attr1", "It is 160 seconds late");
+		attr.put("ID", "7");
 		attr.put("Attr2", "changed on that day");
 		attr.put("Attr3", "160 degrees to the left");
 		pivots.put("Pivot 7", attr);
