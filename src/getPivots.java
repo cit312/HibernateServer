12d5ca50eb1bc52ab25359a2bf9ed7f1764aa201
brayden hancock <brayden.hancock@tvw.org>
2013-07-06 04:29:10 -0700

    Changed artificial pivot data so I could make sure getting correct data

--- src/getPivots.java
+++ src/getPivots.java
@@ -12,44 +12,44 @@ public class GetPivots implements Handler {
 		HashMap<String,HashMap> pivots = new HashMap<String,HashMap>();
 		HashMap<String,String> attr = new HashMap<String,String>();
 		attr.put("ID", "1");
-		attr.put("Attr2", "Last changed on that day");
-		attr.put("Attr3", "60 degrees to the left");
+		attr.put("Attr2", "1Last changed on that day");
+		attr.put("Attr3", "160 degrees to the left");
 		pivots.put("Pivot 1", attr);
 		
 		attr = new HashMap<String,String>();
 		attr.put("ID", "2");
-		attr.put("Attr2", "changed on that day");
-		attr.put("Attr3", "160 degrees to the left");
+		attr.put("Attr2", "2changed on that day");
+		attr.put("Attr3", "2160 degrees to the left");
 		pivots.put("Pivot 2", attr);
 		
 		attr = new HashMap<String,String>();
 		attr.put("ID", "3");
-		attr.put("Attr2", "changed on that day");
-		attr.put("Attr3", "160 degrees to the left");
+		attr.put("Attr2", "3changed on that day");
+		attr.put("Attr3", "3160 degrees to the left");
 		pivots.put("Pivot 3", attr);
 		
 		attr = new HashMap<String,String>();
 		attr.put("ID", "4");
-		attr.put("Attr2", "changed on that day");
-		attr.put("Attr3", "160 degrees to the left");
+		attr.put("Attr2", "4changed on that day");
+		attr.put("Attr3", "4160 degrees to the left");
 		pivots.put("Pivot 4", attr);
 		
 		attr = new HashMap<String,String>();
 		attr.put("ID", "5");
-		attr.put("Attr2", "changed on that day");
-		attr.put("Attr3", "160 degrees to the left");
+		attr.put("Attr2", "5changed on that day");
+		attr.put("Attr3", "5160 degrees to the left");
 		pivots.put("Pivot 5", attr);
 		
 		attr = new HashMap<String,String>();
 		attr.put("ID", "6");
-		attr.put("Attr2", "changed on that day");
-		attr.put("Attr3", "160 degrees to the left");
+		attr.put("Attr2", "6changed on that day");
+		attr.put("Attr3", "6160 degrees to the left");
 		pivots.put("Pivot 6", attr);
 		
 		attr = new HashMap<String,String>();
 		attr.put("ID", "7");
-		attr.put("Attr2", "changed on that day");
-		attr.put("Attr3", "160 degrees to the left");
+		attr.put("Attr2", "7changed on that day");
+		attr.put("Attr3", "7160 degrees to the left");
 		pivots.put("Pivot 7", attr);
 		
 		//Put bean into hashmap
